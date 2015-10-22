68a6c0656c6de9f81bb73da1aac479736a353fd9
TerraSetzler <setzler.terra@gmail.com>
2015-10-22 09:15:52 -0700

    Try again

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## x = matrix() forces the input into a matrix
+## First this function is able to change x in the saved variable, if we decide to change x
+## Then if that happens m will be set to NULL through the whole makeCacheMatrix function
+## That will NULL any previous caches
+## Then get will show x as it is now
+## Setinverse does not calculate the inverse but rather stores the calculation if it happens in another function
+## Getinverse will return the inverse if it has been stored
+## Finally a list is created and stores these values
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     
+   ## Return a matrix that is the inverse of 'x'
 }
